fa756e942d741285f6850bbd95855eb0e4d7ab70
manisha9555 <115913147+manisha9555@users.noreply.github.com>
2023-09-22 02:14:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.json({message:'Hello from docker'});
+})
+app.listen(9000,()=>console.log("server running at 9000"));a
